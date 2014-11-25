adabcac86100ba94d573290078fdbd9a5df415da
Lee T <leetusman@gmail.com>
2014-11-25 18:58:40 -0500

    changed program to python, added intro text

---
+++ setupTurtle.py
@@ -0,0 +1,12 @@
+from turtle import *
+print()
+print('LOGO/Turtle drawing in your terminal via python')
+print('example: forward(distance) e.g. fd(20)')
+print('common commands:')
+print('degrees(), clear(), backward/bk(), up(), color(*args), setheading(angle)')
+print('position(), goto(x,y), radians(), tracer(flag), left/lt(), down()')
+print('begin_fill(), window_width(), setx(xpos), heading(), reset(), forward/fd()')
+print('right/rt(angle), width(width), end_fill(), window_height(), sety(ypos)')
+print('Full reference at https://docs.python.org/2/library/turtle.html')
+
+
